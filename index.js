bc6665742e310dd3d3c6180365decea499e0fc66
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-09-16 09:57:32 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-
+const { isUtf8 } = require('buffer')
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3002
+const fs = require('fs')
 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // get data form file
+  fs.readFile('./tasks', 'utf8', (err, data) =>{
+    if (err){
+      console.error(err);
+      return
+    } 
+    //console.log(data);
+    //console.log(typeof data);
+  const tasks = data.split("\n")
+  res.render('index', {tasks: tasks})
+  });
 })
 
 
