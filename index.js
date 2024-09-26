c8cddd01d5e129b1d79c4e8eeeb7a3bc15dc7c25
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-09-26 15:02:13 +0300

    Update single task data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { isUtf8 } = require('buffer')
+const { error } = require('console')
 const express = require('express')
 const app = express()
 const port = 3003
@@ -108,6 +109,20 @@ app.get('/delete-task/:taskId', (req, res) => {
 })
 
 
+app.get("/edit-task/:taskId", (req, res) => {
+  const editTaskId = parseInt(req.params.taskId);
+  readFile("./tasks.json")
+    .then(tasks => {
+      const taskToEdit = tasks.find(task => task.id === editTaskId);
+      if (taskToEdit) {
+        res.render('edit', { task: taskToEdit, error: null });
+        console.log('Task for updating =>', taskToEdit )
+      } else {
+        res.redirect('/');
+      }
+    });
+ });
+
 // Clear all tasks
 app.post('/clear-tasks', (req, res) => {
   // kirjutab taskidesse task asemel[], mis teeb tasks.json faili tühjaks 
@@ -120,6 +135,51 @@ app.post('/clear-tasks', (req, res) => {
     });
 });
 
+
+app.post("/update-task/:taskId", (req, res) => {
+  const updateTaskId = parseInt(req.params.taskId);
+  const updatedTask = req.body.task.trim();
+ 
+  readFile("./tasks.json")
+    .then(tasks => {
+      const taskIndex = tasks.findIndex(task => task.id === updateTaskId);
+      let error = null
+
+      if (taskIndex !== -1 && updatedTask.length > 0) {
+        tasks[taskIndex].task = updatedTask;
+        const data = JSON.stringify(tasks, null, 2);
+        const taskToEdit = tasks[taskIndex];
+        console.log(taskToEdit)
+        return writeFile("./tasks.json", data);
+
+      } else if (updatedTask.length === 0) {
+        error = 'Please insert correct task data'
+        const taskToEdit = tasks[taskIndex];
+        const data = JSON.stringify(tasks, null, 2);
+        
+          // controll data from form
+          res.render('edit', {
+            task: taskToEdit,  // Pass the task back to the form
+            error: error       // Pass the error message
+          });
+          console.log('Task data from updating =>', taskToEdit )
+          return writeFile("./tasks.json", data);
+
+        } else {
+          throw new Error("Task not found.");
+        }
+    })
+    .then(() => {
+      if (!res.headersSent) {
+        // Only redirect if headers have not been sent already
+        res.redirect("/");
+      }
+    })
+    .catch(err => {
+      console.error(err)
+      res.redirect("/");
+    });
+ });
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
